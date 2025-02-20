bedd4ecb7abb5335f69d4083f7e90d1730721b08
ziyad1412 <abdurrahman.ziyad17@gmail.com>
2025-02-20 10:57:52 +0700

    Materi 4

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 function BlogPost(props) {
+  // State untuk menyimpan jumlah Like
+  const [likes, setLikes] = useState(0);
+
+  // Function untuk menambah Like
+  const handleLike = () => {
+    setLikes(likes + 1);
+  };
+
   return (
     <div className="blog-post">
       <h2>{props.title}</h2>
       <p>{props.content}</p>
+      <button onClick={handleLike}>Like ({likes})</button>
     </div>
   );
 }
